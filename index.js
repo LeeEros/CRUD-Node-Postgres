1e3af6ec1f2463edc21b8a89ceebccd629ac321a
Lee Eros Galaad Dias De Moura <95001466+LeeEros@users.noreply.github.com>
2024-08-26 11:00:59 -0300

    Feat dotenv

---
+++ index.js
@@ -0,0 +1,7 @@
+
+require("dotenv").config();
+
+const port = process.env.PORT;
+
+const express = require('express');
+const app = express();
--- tutorial/queries.js
+++
@@ -1,9 +0,0 @@
-const Conexaobanco = require("pg").Pool;
-
-const pool = new Conexaobanco({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'project',
-    password: 'postgres',
-    port: 5432
-});
