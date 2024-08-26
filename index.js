c215851025b92196ce073ff0b71f6043a9e2bab0
Lee Eros Galaad Dias De Moura <95001466+LeeEros@users.noreply.github.com>
2024-08-26 16:37:17 -0300

    Feat:  Crud Repository

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
 require("dotenv").config();
 
 const port = process.env.PORT;
 
-const express = require('express');
-const app = express();
+app.use(bodyParser.json());
+
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+)
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta de conexão ${port}.`)
+})
+
+app.get('/', (request, response) => {
+    console.log("Hello World!")
+})
